d13467aac0cdceb06176d18ef8977f6384dcf65d
nayanepreta <nayanepreta01@gmail.com>
2025-01-10 13:21:45 -0300

    Add files via upload

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import chapters from '../control/chapters';
 
-
-const Cover = ({ 
-  onNext, 
-  }) => {
-
-    const { 
-      titulo: titulo, 
-      autor: autor,
-      editora: editora} = chapters[0]; 
+const Cover = ({ onNext }) => {
+  const { titulo, autor, editora } = chapters[0];
 
   return (
     <div className="cover">
-      <img className="capa" src="https://raw.githubusercontent.com/nayanepreta/alice-in-wonderland/refs/heads/main/src/assets/capa.png" alt="" />
-      <p className="cover_nome_autor">{autor}</p>
+      <img 
+        className="cover-image" 
+        src="https://raw.githubusercontent.com/nayanepreta/alice-in-wonderland/refs/heads/main/src/assets/capa.png" 
+        alt={`Capa do livro ${titulo}`} 
+        loading="lazy" 
+      />
+
+      <p className="cover-author">{autor}</p>
 
       <img 
-        className="cover_logo_editora" 
+        className="cover-publisher-logo" 
         src="https://via.placeholder.com/25" 
-        alt={editora} loading="lazy"/>
+        alt={`Logo da editora ${editora}`} 
+        loading="lazy" 
+      />
+
       <div className="navigation">
-        <button onClick={onNext}>
+        <button onClick={onNext} aria-label="Próxima página">
           <img
             src="https://raw.githubusercontent.com/nayanesenhorinha/abelhinha/refs/heads/main/src/assets/depois.png"
-            alt="Next"
+            alt="Próxima"
             className="nav-icon"
             loading="lazy"
           />
--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -4,6 +4,7 @@ import { useSwipeable } from 'react-swipeable';
 import '../control/copy';
 import '../styles/ebook.css';
 
+// Componentes de cada parte do livro (capa, sumário, capítulos, etc.)
 import Cover from '../components/Cover';
 import Infos from '../components/Infos';
 import Intro from '../components/Intro';
@@ -25,119 +26,45 @@ import Afterword from '../components/Afterword';
 import Credits from '../components/Credits';
 import Colophon from '../components/Colophon';
 
+// Componente principal do livro
 const Book = () => {
 
+  // Estado que controla a página atual
   const [currentPage, setCurrentPage] = useState(0);
+
+  // Função para atualizar a página atual e salvar no localStorage
   const setCurrentPageAndSave = (page) => {
     if (page !== currentPage) {
       setCurrentPage(page);
       localStorage.setItem('currentPage', page); 
     }
   };
-  
+
+  // Definição de todas as páginas do livro, cada uma representada por um componente
   const pages = [
-    <Cover  // 0
-      goToSummary={() => setCurrentPageAndSave(3)}
-      onNext={() => setCurrentPageAndSave(1)} />,
-
-    <Infos // 1
-      onNext={() => setCurrentPageAndSave(2)} 
-      goToCover={() => setCurrentPageAndSave(0)}
-      goToSummary={() => setCurrentPageAndSave(3)} />,
-
-    <Intro // 2
-      onNext={() => setCurrentPageAndSave(3)} 
-      goToCover={() => setCurrentPageAndSave(0)}
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-
-    <Summary // 3
-      onNext={() => setCurrentPageAndSave(4)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToChapter={(pageIndex) => setCurrentPageAndSave(pageIndex)}/>,
-
-    <Preface // 4
-      onNext={() => setCurrentPageAndSave(5)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-
-    <Chapter1 // 5
-      onNext={() => setCurrentPageAndSave(6)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-      
-    <Chapter2 // 6
-      onNext={() => setCurrentPageAndSave(7)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-    
-    <Chapter3 // 7
-      onNext={() => setCurrentPageAndSave(8)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-    
-    <Chapter4 // 8
-      onNext={() => setCurrentPageAndSave(9)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-
-    <Chapter5 // 9
-      onNext={() => setCurrentPageAndSave(10)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-
-
-    <Chapter6 // 10
-      onNext={() => setCurrentPageAndSave(11)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-
-
-    <Chapter7 // 11
-      onNext={() => setCurrentPageAndSave(12)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-
-
-    <Chapter8 // 12
-      onNext={() => setCurrentPageAndSave(13)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-
-    <Chapter9 // 13
-      onNext={() => setCurrentPageAndSave(14)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-
-    <Chapter10 // 14
-      onNext={() => setCurrentPageAndSave(15)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-
-    <Chapter11 // 15
-      onNext={() => setCurrentPageAndSave(16)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-    
-    <Chapter12 // 16
-      onNext={() => setCurrentPageAndSave(17)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-
-    <Afterword // 17
-      onNext={() => setCurrentPageAndSave(18)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-
-    <Credits // 18
-      onNext={() => setCurrentPageAndSave(19)} 
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
-
-    <Colophon // 19
-      goToCover={() => setCurrentPageAndSave(0)} 
-      goToSummary={() => setCurrentPageAndSave(3)}/>,
+    <Cover onNext={() => setCurrentPageAndSave(1)} goToSummary={() => setCurrentPageAndSave(3)} />,
+    <Infos onNext={() => setCurrentPageAndSave(2)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)} />,
+    <Intro onNext={() => setCurrentPageAndSave(3)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>,
+    <Summary onNext={() => setCurrentPageAndSave(4)} goToCover={() => setCurrentPageAndSave(0)} goToChapter={(pageIndex) => setCurrentPageAndSave(pageIndex)}/>,
+    <Preface onNext={() => setCurrentPageAndSave(5)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>,
+    <Chapter1 onNext={() => setCurrentPageAndSave(6)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>,
+    <Chapter2 onNext={() => setCurrentPageAndSave(7)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>,
+    <Chapter3 onNext={() => setCurrentPageAndSave(8)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>,
+    <Chapter4 onNext={() => setCurrentPageAndSave(9)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>,
+    <Chapter5 onNext={() => setCurrentPageAndSave(10)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>,
+    <Chapter6 onNext={() => setCurrentPageAndSave(11)}  goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>,
+    <Chapter7 onNext={() => setCurrentPageAndSave(12)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>, 
+    <Chapter8 onNext={() => setCurrentPageAndSave(13)}  goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>,
+    <Chapter9 onNext={() => setCurrentPageAndSave(14)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>, 
+    <Chapter10 onNext={() => setCurrentPageAndSave(15)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>,
+    <Chapter11 onNext={() => setCurrentPageAndSave(16)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>,
+    <Chapter12 onNext={() => setCurrentPageAndSave(17)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>, 
+    <Afterword onNext={() => setCurrentPageAndSave(18)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>, 
+    <Credits onNext={() => setCurrentPageAndSave(19)} goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>,
+    <Colophon goToCover={() => setCurrentPageAndSave(0)} goToSummary={() => setCurrentPageAndSave(3)}/>
   ];
 
+  // Recupera a página salva no localStorage quando o componente é montado
   useEffect(() => {
     const savedPage = localStorage.getItem('currentPage');
     if (savedPage) {
@@ -145,18 +72,21 @@ const Book = () => {
     }
   }, []);
 
+  // Função para lidar com o deslizar para a esquerda (avançar)
   const handleSwipeLeft = () => {
     if (currentPage < pages.length - 1) {
       setCurrentPageAndSave(currentPage + 1);
     }
   };
 
+  // Função para lidar com o deslizar para a direita (voltar)
   const handleSwipeRight = () => {
-    if (currentPage > 0) {
-      setCurrentPageAndSave(currentPage - 1);
+    if (currentPage > 0) { 
+      setCurrentPageAndSave(currentPage - 1); 
     }
   };
 
+   // Configura o hook de gestos de deslizar
   const swipeHandlers = useSwipeable({
     onSwipedLeft: handleSwipeLeft,
     onSwipedRight: handleSwipeRight,
@@ -164,6 +94,7 @@ const Book = () => {
     trackMouse: true,
   });
 
+  // Renderiza a página atual, aplicando os gestos de deslizar
   return (
     <div {...swipeHandlers}>
       {pages[currentPage]}
